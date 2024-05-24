eb8697a2ec4df0a411585948f445c432c82c3622
Fajar Kamaludin Akhmad <97646225+fjrUdn@users.noreply.github.com>
2024-05-24 15:13:13 +0700

    Add files via upload

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,163 +1,121 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+import numpy as np
 import streamlit as st
-from streamlit_folium import st_folium
-from babel.numbers import format_currency
-import folium
-from streamlit_folium import folium_static
-import base64
-from io import BytesIO
-sns.set(style='dark')
+from keras.models import load_model
+from sklearn.preprocessing import MinMaxScaler
 
 st.set_page_config(layout="wide")
-# Load data
-def load_and_prepare_data(file_path, date_columns):
-    # Memuat data dari file Excel
-    df = pd.read_excel(file_path)
-    df.sort_values(by=date_columns, inplace=True)
-    df.reset_index(inplace=True)
-    for column in date_columns:
-        df[column] = pd.to_datetime(df[column])
+st.header('Model Peramalan Harga Daging Ayam :sparkles:')
+
+# Load Model 
+loaded_model = load_model("bestModel_lstm.h5")
+
+# Prepare Data
+df = pd.read_csv('data_daging_ayam_clean23.csv')
+df['tanggal'] = pd.to_datetime(df['tanggal'])
+df = df.set_index('tanggal')
+
+df1 = pd.read_csv('data_bawang_merah_clean23.csv')
+df1['tanggal'] = pd.to_datetime(df1['tanggal'])
+df1 = df1.set_index('tanggal')
+
+scale = MinMaxScaler()
+df['pasar manis'] = scale.fit_transform(df[['pasar manis']])
+df['pasar wage'] = scale.fit_transform(df[['pasar wage']])
+df1['pasar manis'] = scale.fit_transform(df1[['pasar manis']])
+df1['pasar wage'] = scale.fit_transform(df1[['pasar wage']])
+
+min_val = scale.data_min_
+max_val = scale.data_max_
+
+# Make Prediction
+def forecast_data(df, column_name, loaded_model, scale, forecast_steps=93):
+    look_back=1
+    historical_data_pm = df[column_name].iloc[0:]
+
+    X_forecast = []
+    y_forecast = []
+
+    for i in range(len(historical_data_pm.values.reshape(len(historical_data_pm), 1)) - look_back):
+        a = historical_data_pm.values.reshape(len(historical_data_pm), 1)[i:(i + look_back), 0]
+        X_forecast.append(a)
+        y_forecast.append(historical_data_pm.values.reshape(len(historical_data_pm), 1)[i + look_back, 0])
+
+    X_forecast = np.array(X_forecast)
+    y_forecast = np.array(y_forecast)
+
+    X_forecast = X_forecast.reshape(X_forecast.shape[0], 1, X_forecast.shape[1])
+    pred_forecast = loaded_model.predict(X_forecast[-forecast_steps:])  # Use last X values to forecast future steps
+
+    # Append forecasted values to historical data
+    forecasted_values = []
+    for i in range(forecast_steps):
+        next_input = np.append(X_forecast[-1, 0, 1:], pred_forecast[i])  # Assuming input sequence length is 5
+        next_input = next_input.reshape(1, 1, look_back)  # Reshape for LSTM model input
+
+        # Predict the next value
+        next_pred = loaded_model.predict(next_input)
+        forecasted_values.append(next_pred[0])
+
+    last_date = df.index[-1]  # Ambil tanggal terakhir dalam data
+    future_index = pd.date_range(start=last_date + pd.DateOffset(days=1), periods=forecast_steps, freq='D')
+    forecast_df = pd.DataFrame({'Forecast': np.array(forecasted_values).flatten()}, index=future_index)
+
+    historical_data_pm_denormalized = scale.inverse_transform(historical_data_pm.values.reshape(-1, 1)).flatten()
+
+    forecasted_values_denormalized = scale.inverse_transform(forecast_df['Forecast'].values.reshape(-1, 1)).flatten()
+
+    historical_data_pm_denorm_df = pd.DataFrame({'Historical Data': historical_data_pm_denormalized}, index=historical_data_pm.index)
+    forecasted_values_denorm_pm_df = pd.DataFrame({'Forecast': forecasted_values_denormalized}, index=forecast_df.index)
+
+    combined_denorm_df = pd.concat([historical_data_pm_denorm_df, forecasted_values_denorm_pm_df])
+
+    return combined_denorm_df
+
+# Save Data Prediction
+def merge_forecast_data(combined_denorm_df_pm, combined_denorm_df_pw):
+    # Convert to DataFrames
+    data_df_pm = pd.DataFrame(combined_denorm_df_pm)
+    data_df_pw = pd.DataFrame(combined_denorm_df_pw)
+
+    # Add "Pasar Manis" column by filling values from "Historical Data" and "Forecast"
+    data_df_pm['Pasar Manis'] = data_df_pm['Historical Data'].combine_first(data_df_pm['Forecast'])
+
+    # Add "Pasar Wage" column by filling values from "Historical Data" and "Forecast"
+    data_df_pw['Pasar Wage'] = data_df_pw['Historical Data'].combine_first(data_df_pw['Forecast'])
+
+    # Create "Keterangan" column
+    data_df_pm['Keterangan'] = ['Historical Data' if pd.notna(x) else 'Forecast' for x in data_df_pm['Historical Data']]
+
+    # Merge DataFrames based on index
+    final_df = pd.DataFrame({
+        'Date': data_df_pm.index,
+        'Pasar Manis': data_df_pm['Pasar Manis'],
+        'Pasar Wage': data_df_pw['Pasar Wage'],
+        'Keterangan': data_df_pm['Keterangan']
+    })
+
+    # Reset index
+    final_df = final_df.reset_index(drop=True)
     
-    return df
+    return final_df
 
-df_pm = load_and_prepare_data('dashboard/data_bawang_merah_pm.xlsx', ["Date"])
-df_pw = load_and_prepare_data('dashboard/data_bawang_merah_pw.xlsx', ["Date"])
-df_gab = load_and_prepare_data('dashboard/data_bawang_merah.xlsx', ["Date"])
+# Main
+def main():
+    st.write("Jumlah hari yang akan diprediksi : ", number)
+    st.write("Forecast data....")
+    ayam_pm = forecast_data(df,'pasar manis', loaded_model, scale, number) 
+    ayam_pw = forecast_data(df,'pasar wage', loaded_model, scale, number)
+    bawang_pm = forecast_data(df1,'pasar manis', loaded_model, scale, number)
+    st.write("Hampir selesai....")
+    bawang_pw = forecast_data(df1,'pasar wage', loaded_model, scale, number)
+    ayam_gab = merge_forecast_data(ayam_pm, ayam_pw)
+    bawang_gab = merge_forecast_data(bawang_pm, bawang_pw)
+    ayam_gab.to_excel("data_daging_ayam.xlsx")
+    bawang_gab.to_excel("data_bawang_merah.xlsx")
+    st.write("Sukses")
 
-min_date = df_pm["Date"].min()
-max_date = df_pm["Date"].max()
-
-# sidebar sebelah kiri
-with st.sidebar:
-    # Menambahkan logo perusahaan
-    #st.image("https://github.com/dicodingacademy/assets/raw/main/logo.png")
-    
-    # Mengambil start_date & end_date dari date_input
-    start_date, end_date = st.date_input(
-        label='Rentang Waktu',min_value=min_date,
-        max_value=max_date,
-        value=[min_date, max_date]
-    )
-
-df1 = df_pm[(df_pm["Date"] >= str(start_date)) & 
-                (df_pm["Date"] <= str(end_date))]
-df1['Date'] = pd.to_datetime(df1['Date'])
-
-df_pm.set_index('Date', inplace=True)
-
-df2 = df_pw[(df_pw["Date"] >= str(start_date)) & 
-                (df_pw["Date"] <= str(end_date))]
-df2['Date'] = pd.to_datetime(df2['Date'])
-
-df_pw.set_index('Date', inplace=True)
-
-df3 = df_gab[(df_gab["Date"] >= str(start_date)) & 
-                (df_gab["Date"] <= str(end_date))]
-df3['Date'] = pd.to_datetime(df3['Date'])
-
-df_gab.set_index('Date', inplace=True)
-
-# Header 
-st.header('Forecast Harga Bawang Merah :sparkles:')
-
-# Plot Forecast Marker
-fig, ax = plt.subplots(figsize=(7, 3))
-ax.plot(
-    df_pm.index,
-    df_pm['Forecast'],
-    color='red',
-    label='Forecast', 
-    linestyle='--', 
-    #linewidth=2
-)
-ax.set_title('Forecast Pasar Manis', fontsize=15)
-ax.tick_params(axis='y', labelsize=7)
-ax.tick_params(axis='x', labelsize=7, rotation=17)
-
-image_stream = BytesIO()
-plt.savefig(image_stream, format='png')
-plt.close()
-
-image_base64 = base64.b64encode(image_stream.getvalue()).decode("utf-8")
-html = f'<img src="data:image/png;base64,{image_base64}">'
-max_width = 1000
-popup_manis = folium.Popup(html, max_width=max_width)
-
-# Plot Forecast Marker
-fig, ax = plt.subplots(figsize=(7, 3))
-ax.plot(
-    df_pw.index,
-    df_pw['Forecast'],
-    color='red',
-    label='Forecast', 
-    linestyle='--', 
-    #linewidth=2
-)
-ax.set_title('Forecast Pasar Wage', fontsize=15)
-ax.tick_params(axis='y', labelsize=7)
-ax.tick_params(axis='x', labelsize=7, rotation=17)
-
-image_stream = BytesIO()
-plt.savefig(image_stream, format='png')
-plt.close()
-
-image_base64 = base64.b64encode(image_stream.getvalue()).decode("utf-8")
-html = f'<img src="data:image/png;base64,{image_base64}">'
-max_width = 1000
-popup_wage = folium.Popup(html, max_width=max_width)
-
-# Create Folium map
-m = folium.Map(location=[-7.4205726027999, 109.24285399533692], zoom_start=15, width='100%', height='100%')
-folium.Marker([-7.417745006891739, 109.22726059533683], popup=popup_manis, icon=folium.Icon(color='blue', icon='location', prefix='fa-solid fa-shop'), tooltip=str('Pasar Manis')).add_to(m)
-folium.Marker([-7.426524254740998, 109.24983460883072], popup=popup_wage, icon=folium.Icon(color='blue', icon='location', prefix='fa-solid fa-shop'), tooltip=str('Pasar Wage')).add_to(m)
-
-with st.expander("Find market on maps", expanded=True):
-    st.subheader('Map')
-    st_folium(m, width=2000, height=450)
-
-# st_folium(m, width=2000, height=450)
-# st.pyplot(fig)
-
-# st.subheader('List Harga Pasar Manis')
-# # tabel bawah
-# df1['Date'] = pd.to_datetime(df1['Date'])
-# # Mengonversi kolom 'Date' menjadi format string tanpa waktu
-# df1['Date'] = df1['Date'].dt.strftime('%Y-%m-%d')
-
-# # Mengonversi kolom Currency menjadi format mata uang Rupiah
-# df1['Historical Data'] = df1['Historical Data'].apply(lambda x: locale.currency(x, grouping=True) if pd.notnull(x) else x)
-# df1['Forecast'] = df1['Forecast'].apply(lambda x: locale.currency(x, grouping=True)if pd.notnull(x) else x)
-
-# st.dataframe(df1.iloc[:,1:], use_container_width=True, hide_index=True)
-
-# st.subheader('List Harga Pasar Wage')
-# # tabel bawah
-# df2['Date'] = pd.to_datetime(df2['Date'])
-# # Mengonversi kolom 'Date' menjadi format string tanpa waktu
-# df2['Date'] = df2['Date'].dt.strftime('%Y-%m-%d')
-
-# # Mengonversi kolom Currency menjadi format mata uang Rupiah
-# df2['Historical Data'] = df2['Historical Data'].apply(lambda x: locale.currency(x, grouping=True) if pd.notnull(x) else x)
-# df2['Forecast'] = df2['Forecast'].apply(lambda x: locale.currency(x, grouping=True)if pd.notnull(x) else x)
-
-# st.dataframe(df2.iloc[:,1:], use_container_width=True, hide_index=True)
-##-----------------------------------------
-st.subheader('List Harga')
-# tabel bawah
-df3['Date'] = pd.to_datetime(df3['Date'])
-# Mengonversi kolom 'Date' menjadi format string tanpa waktu
-df3['Date'] = df3['Date'].dt.strftime('%Y-%m-%d')
-
-# Mengonversi kolom Currency menjadi format mata uang Rupiah
-def format_rupiah(x):
-    if pd.notnull(x):
-        return 'Rp {:,.2f}'.format(x).replace(',', '.')
-    else:
-        return x
-
-df3['Pasar Manis'] = df3['Pasar Manis'].apply(format_rupiah)
-df3['Pasar Wage'] = df3['Pasar Wage'].apply(format_rupiah)
-
-st.dataframe(df3.iloc[:,1:], use_container_width=True, hide_index=True)
+if __name__ == "__main__":
+    number = st.number_input("Insert a number", value=0)
+    if number > 0:
+        main()
